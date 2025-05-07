c04f708a021c957aec3c3553fccffb0f2d554f94
Artem Lytvyn <122204149+letv1nnn@users.noreply.github.com>
2025-05-07 13:32:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
 #include "common.h"
 #include "chunk.h"
+#include "virtualMachine.h"
 
 
 int main(int argc, char **argv) {
+	VirtualMachine vm;
+	initVM(&vm);
+
 	Chunk chunk;
 	createChunk(&chunk);
 
-	writeConstant(&chunk, 123.456, 90);
-	writeConstant(&chunk, 99.88, 1);
-	writeChunk(&chunk, OP_RETURN, 12);
-	displayChunk(&chunk);
+	writeConstant(&chunk, 1.2, 1);
+	writeConstant(&chunk, 3.4, 1);
+	writeChunk(&chunk, OP_ADD, 1);
+	writeConstant(&chunk, 5.6, 1);
+	writeChunk(&chunk, OP_DIV, 1);
+	writeChunk(&chunk, OP_NEG, 2);
+	writeChunk(&chunk, OP_RETURN, 3);
+	// displayChunk(&chunk);
+
+	interpret(&vm, &chunk);
 
 	freeChunk(&chunk);
+	freeVM(&vm);
 
 	return 0;
 }
