49c1039c3a84052eeb95aa92faf1a67600f40742
Artem Lytvyn <122204149+letv1nnn@users.noreply.github.com>
2025-05-09 00:52:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,27 +2,76 @@
 #include "chunk.h"
 #include "virtualMachine.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void repl(VirtualMachine vm) {
+	char line[1024];
+	for (;;) {
+		printf("> ");
+
+		if (!fgets(line, sizeof(line), stdin)) {
+			printf("\n");
+			break;
+		}
+
+		interpret(&vm, line);
+	}
+}
+
+static char* readFile(const char* path) {
+	FILE* file = fopen(path, "rb");
+	if (file == NULL) {
+		fprintf(stderr, "Could not open file \"%s\".\n", path);
+		exit(74);
+	}
+
+	fseek(file, 0L, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+
+	char* buffer = (char*)malloc(fileSize + 1);
+	if (buffer == NULL) {
+		fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+		exit(74);
+	}
+	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+	if (bytesRead < fileSize) {
+		fprintf(stderr, "Could not read file \"%s\".\n", path);
+		exit(74);
+	}
+	buffer[bytesRead] = '\0';
+
+	fclose(file);
+	return buffer;
+}
+
+//take care of loading scripts
+static void runFile(VirtualMachine vm, const char *path) {
+	char* source = readFile(path);
+	InterpretResult result = interpret(&vm, source);
+	free(source);
+
+	if (result == INTERPRET_COMPILE_ERROR) exit(65);
+	if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
 
 int main(int argc, char **argv) {
 	VirtualMachine vm;
 	initVM(&vm);
 
-	Chunk chunk;
-	createChunk(&chunk);
-
-	writeConstant(&chunk, 1.2, 1);
-	writeConstant(&chunk, 3.4, 1);
-	writeChunk(&chunk, OP_ADD, 1);
-	writeConstant(&chunk, 5.6, 1);
-	writeChunk(&chunk, OP_DIV, 1);
-	writeChunk(&chunk, OP_NEG, 2);
-	writeChunk(&chunk, OP_RETURN, 3);
-	// displayChunk(&chunk);
-
-	interpret(&vm, &chunk);
-
-	freeChunk(&chunk);
+	if (argc == 1) {
+		repl(vm); // ingle command line argument
+	}
+	else if (argc == 2) {
+		runFile(vm, argv[1]);
+	}
+	else {
+		fprintf(stderr, "Usage: VERTEL [path]\n");
+		exit(64);
+	}
+	
 	freeVM(&vm);
-
 	return 0;
 }
