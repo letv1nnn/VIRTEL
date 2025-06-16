ff9fdf470549b9ea5ce6a23a3f7990791adcddcc
Artem <122204149+letv1nnn@users.noreply.github.com>
2025-06-16 21:12:24 +0100

    Add files via upload

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,5 @@
 
+use std::collections::HashMap;
 
 pub enum TokenType {
     LeftParen, RightParen,
@@ -28,5 +29,42 @@ pub struct Token {
     pub line: usize,
 }
 
+pub struct Scanner {
+    source: String,
+    start: usize,
+    current: usize,
+    line: usize,
+    keywords: HashMap<String, TokenType>,
+}
 
+impl Scanner {
+    pub fn new(source: String) -> Self {
+        let mut keywords = HashMap::new();
+        keywords.insert("if".to_string(), TokenType::If);
+        keywords.insert("else".to_string(), TokenType::Else);
+        keywords.insert("for".to_string(), TokenType::For);
+        keywords.insert("while".to_string(), TokenType::While);
+        keywords.insert("and".to_string(), TokenType::And);
+        keywords.insert("or".to_string(), TokenType::Or);
+        keywords.insert("true".to_string(), TokenType::True);
+        keywords.insert("false".to_string(), TokenType::False);
+        keywords.insert("noth".to_string(), TokenType::Nil);
+        keywords.insert("fun".to_string(), TokenType::Fun);
+        keywords.insert("var".to_string(), TokenType::Var);
+        keywords.insert("print".to_string(), TokenType::Print);
+        keywords.insert("return".to_string(), TokenType::Return);
+
+        Scanner {
+            source,
+            start: 0,
+            current: 0,
+            line: 1,
+            keywords,
+        }
+    }
+
+    pub fn scan_token(&mut self) -> Token {
+
+    }
+}
 
